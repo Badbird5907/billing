c6930df48b595fa7fe8315ca198d397b8a9bf27c
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2023-06-11 18:33:59 -0400

    403 page

--- src/util/authenticated.ts
+++ src/util/authenticated.ts
@@ -4,22 +4,6 @@ import {LogtoContext} from "@logto/next";
 import {ensureValueNotUndefined} from "@/util/util";
 import {GetContextParameters} from "@logto/node";
 
-export const authenticated = <P extends Record<string, unknown> = Record<string, unknown>>(handler: (context: GetServerSidePropsContext, user: LogtoContext) => GetServerSidePropsResult<P>) => {
-    return logtoClient.withLogtoSsr((context) => {
-        const {user} = context.req;
-        const {res} = context;
-
-        if (!user.isAuthenticated) {
-            res.setHeader('location', '/api/logto/sign-in');
-            res.statusCode = 302;
-            res.end();
-        }
-        return handler(context, ensureValueNotUndefined(user));
-    }, {
-        resource: process.env.LOGTO_API_RESOURCE,
-        getAccessToken: true
-    });
-}
 export const authenticatedApi = (handler: NextApiHandler, scopes: string[] = []) => {
     return logtoClient.withLogtoApiRoute(async (request, response) => {
         if (!request.user.isAuthenticated) {
@@ -48,4 +32,39 @@ export const authenticatedApi = (handler: NextApiHandler, scopes: string[] = [])
         resource: process.env.LOGTO_API_RESOURCE,
         getAccessToken: true
     })
+}
+
+export const authenticated = <P extends Record<string, unknown> = Record<string, unknown>>(handler: (context: GetServerSidePropsContext, user: LogtoContext) => GetServerSidePropsResult<P>, scopes: string[] = []) => {
+    return logtoClient.withLogtoSsr((context) => {
+        const {user} = context.req;
+        const {res} = context;
+        if (!user.isAuthenticated) {
+            res.setHeader('location', '/api/logto/sign-in');
+            res.statusCode = 302;
+            res.end();
+        }
+        // check scopes
+        const defaultScopes = (process.env.LOGTO_API_SCOPE?.split(",") || []) as string[]
+        // add default scopes, removing duplicates
+        const scopesSet = new Set([...scopes, ...defaultScopes])
+        const newScopes = Array.from(scopesSet)
+        if (newScopes.length > 0) {
+            const userScopes = user.scopes;
+            if (!userScopes) {
+                res.setHeader('location', '/403');
+                res.statusCode = 302;
+                res.end();
+            }
+            const hasScope = newScopes.every(scope => userScopes?.includes(scope))
+            if (!hasScope) {
+                res.setHeader('location', '/403');
+                res.statusCode = 302;
+                res.end();
+            }
+        }
+        return handler(context, ensureValueNotUndefined(user));
+    }, {
+        resource: process.env.LOGTO_API_RESOURCE,
+        getAccessToken: true
+    });
 }
