147bae2dab00eb4b64aa26de13d24ed2d6d80994
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2023-06-11 16:10:37 -0400

    use .env

--- src/libraries/config.ts
+++ src/libraries/config.ts
@@ -1,8 +1,8 @@
 export const config = {
-    endpoint: 'https://u0rb1q.logto.app/',
-    appId: 'ooe34zt43u1pxwaxmkz6j',
-    appSecret: '2gso9wokqw59g3o93ajcp',
-    baseUrl: 'http://localhost:3000', // Change to your own base URL
-    cookieSecret: 'gh58xbebwire6a71rz7z0g6waw3rxmjh', // Auto-generated 32 digit secret
+    endpoint: process.env.LOGTO_ENDPOINT || "",
+    appId: process.env.LOGTO_APP_ID || "",
+    appSecret: process.env.LOGTO_APP_SECRET || "",
+    baseUrl: process.env.BASE_URL || "http://localhost:3000",
+    cookieSecret: process.env.LOGTO_COOKIE_SECRET || "logto-cookie-secret",
     cookieSecure: process.env.NODE_ENV === 'production',
 }
